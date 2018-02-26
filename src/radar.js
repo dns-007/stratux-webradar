b01c73725817edc0b25d09fb05b89873446ccc3a
jw <jw@jw-PC>
2018-02-26 21:46:00 +0100

    - Limit 20 traffic auf 20 NM distance
    - Cleanup Radarschirm von alten Zielen

--- src/radar.js
+++ src/radar.js
@@ -1,15 +1,19 @@
 // Definieren einiger Basisvariablen
 var Lat;
 var Long;
-var stratuxip = "192.168.10.1";
+var stratuxip = "192.168.2.89";
 var trafficuri_ws = "ws://" + stratuxip + "/traffic";
 var statusuri = "http://" + stratuxip + "/getStatus";
 var situationuri = "http://" + stratuxip + "/getSituation";
 
+var rotate = 0;
+// Liste aller icao Addressen auf dem Radar
+var shownIcaos = [];
+
 // Laden der Init-Funktion nach Laden der Seite, Interval für Akutalisierungen
 window.addEventListener("load", init, false);
 window.setInterval(basics, 500);
-
+window.setInterval(cleanUp, 2000);
 
 // Ein- und Ausklappen der Details
 function collapseupperbox(mode)
@@ -47,35 +51,53 @@ function gettraffic()
 // Empfang von Traffic via WS
 function onMessage(evt)
 {
- 
+
    // Abstand nullen
    var abstandlat = 0;
    var abstandlng = 0;
    var data = JSON.parse(evt.data);
 
+
+   // alles was weiter als 20 NM ist, verwerfen wir
+    if(data.Position_valid){
+            var distInMiles =  Math.round(Math.round(data.Distance) / 1000 / 1.852);
+            if(distInMiles > 20){
+                    return;
+            }
+    }
+
+
    // Definieren einer sinnvollen Kennung
    var zeichen = data.Tail;
    if (zeichen == "")
    {
-	   zeichen = data.Icao_addr.toString(16).toUpperCase();
+      zeichen = data.Icao_addr.toString(16).toUpperCase();
    }
 
    // Abhängig davon ob das Flugzeug bereits existiert wird es angelegt oder nur ausgewählt
    if (document.getElementById(data.Icao_addr))
    {
       var planex = document.getElementById(data.Icao_addr);
+
    }
    else
    {
       var planex = document.createElement("div");
       planex.id = data.Icao_addr;
       maincircle.appendChild(planex);
+      // Wir merken uns die Icao Addresse um das Element wieder zu finden
+      shownIcaos.push(data.Icao_addr);
    }
+
+   // Timestamp des letzten events auf dem Element setzten
+   planex.setAttribute("millis", Date.now());
+
+
 
    // Flugzeuge ohne Positionsangaben die Kreise anhand ihrer Stärke erhalten
    if (data.Lat == 0)
    {
-	  // Fallback auf genulltes Hintergrundbild falls ein Flugzeug plötzlich keine Posiion mehr sendet
+          // Fallback auf genulltes Hintergrundbild falls ein Flugzeug plötzlich keine Posiion mehr sendet
       planex.style.backgroundImage = "";
       planex.className = "planecircle";
       planex.style.width = (Math.round(data.SignalLevel) * (-20));
@@ -103,6 +125,23 @@ function onMessage(evt)
       planex.innerHTML = "<div class='planeslabel'>" + zeichen + "<br>" + data.Speed + " kts<br>" + data.Alt + " ft</div>";
    }
 
+
+
+}
+
+function cleanUp(){
+           for (i = 0; i < shownIcaos.length; i++) {
+           var icao = shownIcaos[i];
+           var element12 = document.getElementById(icao);
+           if(element12){
+               var millis = Number(element12.getAttribute("millis"));
+               if(( +millis + 40000) < Date.now() ){
+                       element12.remove();
+                       shownIcaos.splice(i, 1);
+               }
+           }
+       }
+
 }
 
 // Abfrage der eigenen Daten und drehen des Radars
@@ -126,7 +165,7 @@ function basics()
       document.getElementById('Heading').innerHTML = "Heading: " + Math.round(datasituation.GPSTrueCourse) + ' °';
       document.getElementById('AHRSMagHeading').innerHTML = "AHRSMagHeading: " + Math.round(datasituation.GPSTrueCourse) + ' °';
       document.getElementById('BaroPressureAltitude').innerHTML = "BaroPressureAltitude: " + Math.round(datasituation.BaroPressureAltitude) + ' ';
-      document.getElementById('GPSAltitude').innerHTML = "GPSAltitude: " + Math.round(datasituation.GPSHeightAboveEllipsoid) + ' ';
+          document.getElementById('GPSAltitude').innerHTML = "GPSAltitude: " + Math.round(datasituation.GPSAltitudeMSL) + ' ';
       document.getElementById('AHRSGLoad').innerHTML = "AHRSGLoad: " + Math.round(datasituation.AHRSGLoad) + ' G';
       Lat = datasituation.GPSLatitude;
       Long = datasituation.GPSLongitude;
@@ -135,3 +174,4 @@ function basics()
    }
    requestsituation.send();
 }
+
